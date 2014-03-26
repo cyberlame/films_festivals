c5223222e58f51a75464fa00b0f2687fb6d3d21a
cyberlame <cyberlame@mail.no>
2014-03-26 15:21:10 +0200

    cleanup code

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,11 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'fests.views.home', name='home'),
     url(r'^fests', 'fests.views.fests', name='fests'),
-    
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 )
